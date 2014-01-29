f596fbe5624606ac00f4b5ecddf3a907b16bf140
Ranjan Sakalley <rsakalle@thoughtworks.com>
2014-01-29 20:41:55 +0530

    last commit here, moving to elpa

--- init.el
+++ init.el
@@ -4,24 +4,22 @@
 (setq visible-bell t)
 (global-font-lock-mode t)
 (setq exec-path (append exec-path '("/opt/local/bin")) )
-(global-visual-line-mode 1)
+;;(global-visual-line-mode 1)
 
 ;;;css mode
 (load-file "~/work/dotemacs/css-mode.el")
 
 ;;;view config
 (load-file "~/work/dotemacs/view.el")
-
 ;;;custom functions
 (load-file "~/work/dotemacs/custom.el")
 
-;;; interactive mode
 (require 'ido)
  (ido-mode t)
 
 ;;; rails support
-(add-to-list 'load-path "~/work/dotemacs/rinari/")
-(require 'rinari)
+;(add-to-list 'load-path "~/work/dotemacs/rinari/")
+;(require 'rinari)
 
 ;;; cedet 
 (add-to-list 'load-path "~/work/dotemacs/cedet")
@@ -36,8 +34,8 @@
 (load-file "~/work/dotemacs/nxhtml-init.el")
 
 ;;;clojure mode
-(add-to-list 'load-path "~/work/dotemacs/clojure-mode")
-(require 'clojure-mode)
+;;(add-to-list 'load-path "~/work/dotemacs/clojure-mode")
+;;(require 'clojure-mode)
 
 ;;;yasnippets
 (add-to-list 'load-path "~/work/dotemacs/yasnippet/")
@@ -65,11 +63,12 @@
 ;;;(load-file "~/work/dotemacs/auto-python.el")
 
 ;;; flymake
-(load-file "~/work/dotemacs/flymake-init.el")
+;;(load-file "~/work/dotemacs/flymake-init.el")
 
 ;;; make emacs use the clipboard
 (setq x-select-enable-clipboard t)
-(setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
+
+(setq interprogram-paste-function 'x-selection-value)
 
 ;;; show line numbers
 (load-file "~/work/dotemacs/linum.el")
@@ -90,26 +89,26 @@
 (add-to-list 'load-path "~/work/dotemacs/color-theme")
 (require 'color-theme)
 (color-theme-initialize)
-;(load-file "~/work/dotemacs/themes/zen-and-art.el")
 (color-theme-clarity)
 
 ;;;muse
-(add-to-list 'load-path "~/work/dotemacs/muse/lisp")
-(load-file "~/work/dotemacs/muse/lisp/muse.el")
-(require 'muse-mode) 
+;;(add-to-list 'load-path "~/work/dotemacs/muse/lisp")
+;;(load-file "~/work/dotemacs/muse/lisp/muse.el")
+;;(require 'muse-mode) 
 
 ;;elpa
-(when
-    (load
-     (expand-file-name "~/work/dotemacs/elpa/package.el"))
-  (package-initialize))
+;;(when
+;;    (load
+;;     (expand-file-name "~/work/dotemacs/elpa/package.el"))
+;;  (package-initialize))
+
 
 ;;;scheme
 (setq scheme-program-name "/Applications/mit-scheme.app/Contents/Resources/mit-scheme")
 
 ;;;f# mode
-(add-to-list 'load-path "~/work/dotemacs/fsharp-mode")
-(load-file "~/work/dotemacs/fsharp-mode/fsharp.el")
+;;(add-to-list 'load-path "~/work/dotemacs/fsharp-mode")
+;;(load-file "~/work/dotemacs/fsharp-mode/fsharp.el")
 
 ;;;backup folder
 (setq
@@ -120,3 +119,27 @@
  kept-new-versions 6
  kept-old-versions 2
  version-control t)       ; use versioned backups
+
+;;erc
+(load-file  "~/work/dotemacs/erc-init.el")
+
+;;coffee
+(load-file "~/work/dotemacs/coffee-mode/coffee-mode.el")
+(define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer)
+(require 'coffee-mode)
+
+
+;;;alt key for console
+;;(setq mac-option-key-is-meta nil)
+;;(setq mac-command-key-is-meta t)
+;;(setq mac-command-modifier 'meta)
+;;(setq mac-option-modifier nil)
+
+
+;; original Emacs Lisp Package Archive
+;;(add-to-list 'package-archives
+  ;;           '("elpa" . "http://tromey.com/elpa/"))
+;; user-contributed repository
+;;(add-to-list 'package-archives
+  ;;           '("marmalade" . "http://marmalade-repo.org/packages/"))
+
