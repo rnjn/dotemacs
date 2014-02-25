2baadcee18fbd857be65920b4e66f90e4fe393f7
Ranjan Sakalley <rsakalle@thoughtworks.com>
2014-02-25 14:04:48 +0530

    Adding python packages, and more

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
 ;;; add packages to be installed on load
 ;;; Code:
 
+;;; Add /usr/local/bin to exec path
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
 ;;list of packages to be installed by elpa
 (defvar installed-packages)
 
@@ -19,6 +22,27 @@
 			   auto-save-buffers-enhanced
 			   magit
 			   flycheck
+			   dired+
+			   project-explorer
+			   yaml-mode
+			   html-to-markdown
+			   markdown-mode+
+			   tidy
+			   slime
+			   ac-slime
+			   clojure-mode
+			   cider
+			   smartparens
+			   rainbow-delimiters
+			   cedet
+			   helm
+			   jedi
+			   iedit
+			   expand-region
+			   python-mode
+			   elpy
+			   flake8
+			   autopair
 			   ))
 
 
@@ -39,8 +63,12 @@
    nil 'fullscreen
    (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
 
+(global-set-key (kbd "C-<f11>") 'toggle-fullscreen)
+
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+			 ("marmalade" . "http://marmalade-repo.org/packages/")))
+;;			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+
 
 ;; init elpa
 (package-initialize)
@@ -52,10 +80,12 @@
 (dolist (package installed-packages)
   (when (and (not (package-installed-p package))
 	     (assoc package package-archive-contents))
+    (message "package is %s" package)
     (package-install package)))
 
+
 ;; color and font
-(load-theme 'solarized-dark t)
+(load-theme 'solarized-light t)
 (set-face-attribute 'default nil :height 140)
 
 ;;; auto complete
@@ -77,7 +107,52 @@
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+;;; cider for clojure
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+;;; smartparens
+(require 'smartparens-config)
+
+;;; rainbow delimiters
+(require 'rainbow-delimiters)
+(global-rainbow-delimiters-mode t)
+
+;;; dired+
+(require 'dired+)
+
+;;; helm
+(require 'helm-config)
+
+;;; revert buffers when backing file changes
+(global-auto-revert-mode t)
+
+;;; project explorer
+(require 'project-explorer-autoloads)
+(set-variable 'pe/width 25)
+
+;;jedi
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
+(require 'jedi)
+
+;;;expand-region
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;;; python
+(elpy-enable)
+
+;;; autopair
+(require 'autopair)
+(autopair-global-mode)
+
 ;;;(provide 'init)
 ;;; init ends here
+
+
+
+
+
 
 
