af8a5a9dc442547853762244039d47933fc5ed8f
Ranjan Sakalley <rsakalle@thoughtworks.com>
2014-04-04 13:22:50 +0530

    python editing customisations

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; package --- Summary
+; package --- Summary
 ;;; Commentary:
 ;;; moving to elpa for package management, use the list that follows to
 ;;; add packages to be installed on load
@@ -43,6 +43,9 @@
 			   elpy
 			   flake8
 			   autopair
+			   ess
+			   fill-column-indicator
+			   simp
 			   ))
 
 
@@ -76,6 +79,8 @@
 ;; refresh contents if not present
 (when (not package-archive-contents) (package-refresh-contents))
 
+(require 'assoc)
+
 ;;; install packages from the list if not installed already
 (dolist (package installed-packages)
   (when (and (not (package-installed-p package))
@@ -85,8 +90,8 @@
 
 
 ;; color and font
-(load-theme 'solarized-light t)
-(set-face-attribute 'default nil :height 140)
+(load-theme 'solarized-dark t)
+(set-face-attribute 'default nil :height 135)
 
 ;;; auto complete
 (require 'auto-complete-config)
@@ -127,8 +132,16 @@
 (global-auto-revert-mode t)
 
 ;;; project explorer
-(require 'project-explorer-autoloads)
-(set-variable 'pe/width 25)
+(require 'project-explorer)
+
+(defun open-project-explorer ()
+  "Custom project explorer configuration."
+  (interactive)
+  (set-variable 'pe/width 25)
+  (setq pe/omit-regex (concat pe/omit-regex "\\|\.pyc$"))
+  (project-explorer-open))
+
+(global-set-key (kbd "C-M-l") 'open-project-explorer)
 
 ;;jedi
 (add-hook 'python-mode-hook 'jedi:setup)
@@ -142,17 +155,63 @@
 
 ;;; python
 (elpy-enable)
+(elpy-use-ipython)
+
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+ "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+ "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
 
 ;;; autopair
 (require 'autopair)
 (autopair-global-mode)
 
+;;; ess
+(require 'ess-site)
+
+;;;fci
+(add-hook 'python-mode-hook 'fci-mode)
+(set-variable 'fci-rule-column 79)
+(set-variable 'fci-rule-use-dashes t)
+(set-variable 'fci-dash-pattern '0.2)
+(set-variable 'fci-rule-color "darkblue")
+(require 'fill-column-indicator)
+
+;; ido
+(require 'ido)
+(ido-mode t)
+
+;; cc
+(require 'cc-mode)
+
+;; flymake keys
+(global-set-key [f2] 'flymake-display-err-menu-for-current-line)
+(global-set-key [f3] 'flymake-goto-prev-error)
+(global-set-key [f4] 'flymake-goto-next-error)
+
+
+(custom-set-faces
+ '(flymake-errline (( ((class color)) (:underline "Red" :background nil)) ))
+ '(flymake-warnline ((((class color)) (:underline "Orange")))))
+
+;;simp
+(require 'simp)
+(simp-project-define
+ '(:has (.git)
+	:ignore (.git *.pyc)))
+(global-set-key (kbd "C-c f") 'simp-project-find-file)
+(global-set-key (kbd "C-c d") 'simp-project-root-dired)
+(global-set-key (kbd "C-c p") 'simp-project-rgrep)
+(global-set-key (kbd "C-c b") 'simp-project-ibuffer-files-only)
+(global-set-key (kbd "C-c B") 'simp-project-ibuffer)
+
 ;;;(provide 'init)
 ;;; init ends here
-
-
-
-
-
-
-
