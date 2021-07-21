1a2fa5877c21018713f3ddd0bb2813762cc480d4
rnjn <runjan@gmail.com>
2021-07-21 21:51:48 +0530

    Refresh. Using use-package and removing much clutter acquired over years.

--- README.md
+++ README.md
@@ -1,39 +1,8 @@
 dotemacs
 ========
-
-my dot emacs setup. uses ELPA to install a bunch of packages if not already installed.
-
-first list packages that need to be installed
-
-``` elisp
-(setq installed-packages '(
-			   color-theme
-			   color-theme-solarized
-			   yasnippet
-			   .......))
-```
-
-initialise list of package archives
-
-``` elisp
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")))
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
-(package-initialize)
-```
-
- refresh contents if not present
-
-``` elisp
-(when (not package-archive-contents) (package-refresh-contents))
-```
-
-install packages from the list if not installed already
-
-``` elisp
-(dolist (package installed-packages)
-  (when (and (not (package-installed-p package))
-	     (assoc package package-archive-contents))
-    (message "package is %s" package)
-    (package-install package)))
-```
+This is a simplistic Emacs setup.  For the last few years (2015-2021) I have been using
+Spacemacs and while it still works and I love the mnemonic keys and layered system
+it has become a slow, and I am tired of waiting for updates, breaking changes.  This is
+an attempt to declutter.  Updating the setup using what's now available in 2021.
+In a bind really whether to write literate configuration, choosing a simple single
+file for now, to avoid the clutter.
--- init.el
+++ init.el
@@ -1,69 +1,315 @@
-; package --- Summary
+;;; rnjns_dotemacs --- Summary
 ;;; Commentary:
-;;; moving to elpa for package management, use the list that follows to
-;;; add packages to be installed on load
+;;; This is a simplistic Emacs setup.  For the last few years (2015-2021) I have been using
+;;; Spacemacs and while it still works and I love the mnemonic keys and layered system
+;;; it has become a slow, and I am tired of waiting for updates, breaking changes.  This is
+;;; an attempt to declutter.  Updating the setup using what's now available in 2021.
+;;; In a bind really whether to write literate configuration, choosing a simple single
+;;; file for now, to avoid the clutter.
 ;;; Code:
 
+;;; Packages
+;;; setup where to pull packages from
+(require 'package)
+
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")
+			 ("melpa" . "http://melpa.org/packages/")))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;;; init use-package. Thanks to @irfn for pointing out use-package;
+;;; it is a great addition. Lazy loading enables a faster setup and
+;;; the added benefit of a clean config. If you are here, you may want to see
+;;; https://github.com/irfn/emacs which is a far better emacs config
+
+(package-install 'use-package)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
 ;;; Add /usr/local/bin to exec path
+(use-package exec-path-from-shell
+  :ensure t
+  :config (setq exec-path-from-shell-variables '("PATH"))
+  :init
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
+
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
+
 ;;list of packages to be installed by elpa
-(defvar installed-packages)
 
-(setq installed-packages '(
-			   color-theme
-			   color-theme-solarized
-			   yasnippet
-			   js2-mode
-			   js2-refactor
-			   fullscreen-mode
-			   auto-save-buffers-enhanced
-			   magit
-			   flycheck
-			   dired+
-			   nav
-			   yaml-mode
-			   html-to-markdown
-			   markdown-mode+
-			   tidy
-			   slime
-			   clojure-mode
-			   cider
-			   smartparens
-			   rainbow-delimiters
-			   cedet
-			   helm
-			   jedi
-			   iedit
-			   expand-region
-			   python-mode
-			   elpy
-			   flake8
-			   autopair
-			   fill-column-indicator
-			   undo-tree
-			   desktop-registry
-			   flymake-jslint
-			   org
-			   haskell-mode
-			   projectile
-			   flx-ido
-			   company-mode
-			   ggtags
-			   perspective
-			   persp-projectile
-			   helm-projectile
-			   zenburn-theme
-			   monokai-theme
-			   popup
-			   scala-mode2
-			   sbt-mode
-			   scala-outline-popup
-			   ensime
-			   ag
-			   helm-ag
-			   ))				
+;;; Fonts and Themes
+(set-face-attribute 'default nil :height 145 :font "Monaco-14")
 
+(use-package solarized-theme
+  :ensure t
+  :defer t)
+
+(use-package material-theme
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+;;; Bells and whistles
+(setq ring-bell-function 'ignore )
+
+(use-package diminish
+  :defer 5
+  :config  (diminish 'org-indent-mode)
+  :ensure t)
+
+
+;;; hints and shortcut discovery
+(use-package which-key
+  :diminish
+  :ensure t
+  :config
+  (which-key-mode)
+  (which-key-setup-side-window-bottom)
+  (setq which-key-idle-delay 0.1))
+
+;;; Helm
+
+(use-package helm
+ :diminish
+ :init
+ (helm-mode t)
+ :bind (("M-x"     . helm-M-x)
+        ("C-x C-f" . helm-find-files)
+        ("C-x b"   . helm-mini)     ;; See buffers & recent files; more useful.
+        ("C-x r b" . helm-filtered-bookmarks)
+        ("C-x C-r" . helm-recentf)  ;; Search for recently edited files
+        ("C-c i"   . helm-imenu)
+        ("C-h a"   . helm-apropos)
+        ("M-y" . helm-show-kill-ring)
+
+        :map helm-map
+        ;; We can list ‘actions’ on the currently selected item by C-z.
+        ("C-z" . helm-select-action)
+        ("TAB"   . helm-execute-persistent-action)
+        ("<tab>" . helm-execute-persistent-action))
+ :config (progn
+	   (setq helm-buffers-fuzzy-matching t)
+	   (setq helm-recentf-fuzzy-matching t)
+	   (setq helm-M-x-fuzzy-match t)
+	   (helm-autoresize-mode 1)
+	  )
+ :ensure t)
+
+;; need for ripgrep speed
+(use-package rg
+  :config
+  (global-set-key (kbd "M-s g") 'rg)
+  (global-set-key (kbd "M-s d") 'rg-dwim)
+  :ensure t)
+
+(use-package helm-rg
+  :ensure t)
+
+;;; Dashboards and Projects
+
+;;; to get a useful homescreen
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-banner-logo-title "oh come on!")
+  (setq dashboard-items '((recents . 5)
+			  (projects . 5)
+			  (agenda . 5)))
+  )
+
+(use-package all-the-icons
+  :ensure t)
+(use-package page-break-lines
+  :ensure t)
+
+(use-package helm-projectile
+  :ensure t)
+
+(use-package projectile
+  :ensure t
+  :diminish
+  :init
+  (projectile-mode +1)
+  :config
+  (setq projectile-project-search-path '("~/Desktop/" "~/work/"))
+  (setq projectile-switch-project-action 'helm-projectile)
+  :bind (:map projectile-mode-map
+	      ("s-p" . projectile-command-map)
+	      ("C-c p" . projectile-command-map)))
+
+;;; Layout
+(global-set-key (kbd "C-<f11>") 'toggle-fullscreen)
+;; fullscreen post init
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+
+(use-package nav
+  :commands nav-toggle nav-disable-overeager-window-splitting
+  :init
+  (bind-key "C-M-l" 'nav-toggle)
+  :ensure t)
+
+(use-package ace-window
+  :delight
+  :bind ("M-o" . ace-window)
+  :config (ace-window-display-mode 1)
+  :ensure t)
+
+;;; hide tool bar
+(if window-system
+    (tool-bar-mode -1)
+)
+;;; show line numbers
+(global-linum-mode 0)
+
+(global-set-key (kbd "s-M-l") 'linum-mode)
+
+
+;;; Finger memory
+;;; cua
+(cua-mode 1)
+
+
+;;; File and folder management
+(setq auto-save-default nil)
+(use-package super-save
+  :ensure t
+  :config
+  (add-to-list 'super-save-triggers 'ace-window)
+  (add-to-list 'super-save-hook-triggers 'find-file-hook)
+  (super-save-mode +1)
+  :diminish)
+
+
+
+
+;;; Org Mode
+
+;;; org-files
+(use-package org
+  :ensure t
+  :bind (("\C-ca" . org-agenda))
+  :config (progn
+            (setq org-todo-keywords (quote ((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "POSTPONED(p)" "|" "CANCELLED(c)"  "DONE(d)"))))
+	    (setq org-agenda-files '("~/Desktop/diary"))
+	    (setq org-log-done 'time)
+            (add-hook 'org-shiftup-final-hook 'windmove-up)
+            (add-hook 'org-shiftleft-final-hook 'windmove-left)
+            (add-hook 'org-shiftdown-final-hook 'windmove-down)
+            (add-hook 'org-shiftright-final-hook 'windmove-right)
+            )
+  :init (progn
+	  (add-hook 'org-mode-hook 'flyspell-mode)
+	  (add-hook 'org-mode-hook 'org-bullets-mode))
+  :custom
+  (org-archive-location "::* Completed")
+  (org-startup-with-inline-images t)
+  (org-modules '(org-crypt
+                 org-habit
+                 org-mouse
+                 org-protocol
+                 org-tempo))
+  (org-tag-alist '(("@research" . ?s)
+                   ("@reading" . ?r)
+                   ("@writing" . ?w)
+                   ("@spike" . ?k)
+                   ("@home" . ?h)
+                   ("@chore" . ?e)
+                   ("@meeting" . ?m)
+                   ("@notes" . ?n)
+                   ("todo" . ?0)
+                   ("@post" . ?p)))
+  )
+
+
+(use-package org-bullets
+  :ensure t
+  :config (setq org-bullets-bullet-list '("◉" "○" "⌗" "⌘" )))
+
+;;; Programming
+
+(use-package flycheck
+  :ensure t
+  :diminish
+  :init
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  :hook (
+	 (prog-mode . flycheck-mode)))
+
+(use-package company
+  :ensure t
+  :init (global-company-mode)
+  :bind ("s-/" . company-complete)
+  :config (
+	   progn
+	    (bind-key [remap-completion-at-point] #'company-complete company-mode-map)
+	    (setq company-tooltip-align-annotations t)
+	    (setq company-dabbrev-downcase t)
+	    (setq company-idle-delay 0)
+            (setq company-minimum-prefix-length 1))
+  :diminish company-mode)
+
+(use-package expand-region
+  :ensure t
+  :bind ("C-=" . er/expand-region))
+
+(use-package yaml-mode
+  :ensure t)
+
+(use-package smartparens
+  :ensure t
+  :config
+  (setq sp-show-pair-from-inside nil)
+  (require 'smartparens-config)
+  :diminish smartparens-mode)
+
+(use-package yasnippet
+  :ensure t
+  :diminish
+  :init
+  (yas-global-mode 1))
+
+
+(use-package magit
+  :ensure t
+  :bind (
+	 ("C-c g" . magit-status))
+  :config (setq magit-auto-revert-mode nil))
+
+;; flymake keys and custom faces
+(global-set-key [f2] 'flymake-display-err-menu-for-current-line)
+(global-set-key [f3] 'flymake-goto-prev-error)
+(global-set-key [f4] 'flymake-goto-next-error)
+
+(custom-set-faces
+ '(flymake-errline (( ((class color)) (:underline "Red" :background nil)) ))
+ '(flymake-warnline ((((class color)) (:underline "Orange")))))
+
+
+
+;;MISC
+;;; revert buffers when backing file changes
+(global-auto-revert-mode t)
+;;; visual line mode
+(global-visual-line-mode t)
+;;; server
+(server-start)
+;;; show me the clock
+(display-time-mode 1)
+(setq display-time-string-forms
+      '((propertize (concat " " 24-hours ":" minutes " "))))
+;;; recentf
+(recentf-mode t)
+
+
+;;; custom functions
 
 (defun reload-dot-emacs ()
   "Reload init file."
@@ -82,325 +328,22 @@
    nil 'fullscreen
    (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
 
-(global-set-key (kbd "C-<f11>") 'toggle-fullscreen)
-
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("org" . "http://orgmode.org/elpa/")
-;			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("melpa" . "http://melpa.org/packages/")))
-
-
-;; init elpa
-(setq package-enable-at-startup nil)
-(package-initialize)
-
-;; refresh contents if not present
-(when (not package-archive-contents) (package-refresh-contents))
-
-;;; install packages from the list if not installed already
-(dolist (package installed-packages)
-  (when (and (not (package-installed-p package))
-	     (assoc package package-archive-contents))
-    (message "package is %s" package)
-    (package-install package)))
-
-;; color and font
-(load-theme 'solarized-dark t t)
-(load-theme 'solarized-light t t)
-(set-face-attribute 'default nil :height 145 :font "Monaco-15")
-;;(enable-theme 'solarized-dark)
- (require 'color-theme)
- (color-theme-initialize)
-;; (color-theme-clarity)
-;; (load-theme 'zenburn t)
-(load-theme 'monokai t)
-
-
-;; delete for terminal
-(normal-erase-is-backspace-mode 1)
-
-;;; auto complete
-;;(require 'auto-complete-config)
-;;(ac-config-default)
-
-;;(defadvice ac-common-setup (after give-yasnippet-highest-priority activate)
-;;  
-;;  (setq ac-sources (delq 'ac-source-yasnippet ac-sources))
-;;  (add-to-list 'ac-sources 'ac-source-yasnippet))
-
-
-;;; hide tool bar
-(if window-system
-    (tool-bar-mode -1)
-)
-
-;;; show line numbers
-(global-linum-mode t)
-
-;;; autosave
-(require 'auto-save-buffers-enhanced)
-  (auto-save-buffers-enhanced t)
-
-(require 'flycheck)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;;; cider for clojure
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-
-;;; smartparens
-(require 'smartparens-config)
-
-;;; rainbow delimiters
-(require 'rainbow-delimiters)
-(global-rainbow-delimiters-mode t)
-
-;;; dired+
-(require 'dired+)
-
-
-;;; revert buffers when backing file changes
-(global-auto-revert-mode t)
-
-;;ls dired issue
-(set-variable 'ls-lisp-use-insert-directory-program nil)
-(require 'ls-lisp)
-
-;;; project explorer
-(require 'nav)
-(nav-disable-overeager-window-splitting)
-;(require 'project-explorer)
-
-;; (defun open-project-explorer ()
-;;   "Custom project explorer configuration."
-;;   (interactive)
-;;   (set-variable 'pe/width 25)
-;;   (set-variable 'pe/cache-enabled 't)
-;;   (set-variable 'pe/directory-tree-function 'pe/get-directory-tree-simple)
-;;   (set-variable 'pe/auto-refresh-cache 'f)
-;;   (setq pe/omit-regex (concat pe/omit-regex "\\|\.pyc$"))
-;;   (project-explorer-open))
-
-(global-set-key (kbd "C-M-l") 'nav-toggle)
-
-
-;;jedi
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t)
-(require 'jedi)
-
-;;;expand-region
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
-
-;;; python
-(elpy-enable)
-
-;;; autopair
-(require 'autopair)
-(autopair-global-mode)
-
-;;; ess
-;(require 'ess-site)
-
-;;;fci
-(add-hook 'python-mode-hook 'fci-mode)
-(set-variable 'fci-rule-column 79)
-(set-variable 'fci-rule-use-dashes t)
-(set-variable 'fci-dash-pattern '0.2)
-(set-variable 'fci-rule-color "darkblue")
-(require 'fill-column-indicator)
-
-;; ido
-(require 'ido)
-(ido-mode t)
-
-;; cc
-(require 'cc-mode)
-
-;; flymake keys
-(global-set-key [f2] 'flymake-display-err-menu-for-current-line)
-(global-set-key [f3] 'flymake-goto-prev-error)
-(global-set-key [f4] 'flymake-goto-next-error)
-
-
-(custom-set-faces
- '(flymake-errline (( ((class color)) (:underline "Red" :background nil)) ))
- '(flymake-warnline ((((class color)) (:underline "Orange")))))
-
-
-;;; magit
-(require 'magit)
-(global-set-key (kbd "C-c g") 'magit-status)
-(setq magit-auto-revert-mode nil)
-(setq magit-last-seen-setup-instructions "1.4.0")
-
-;;; undo tree
-(require 'undo-tree)
-(global-undo-tree-mode 1)
-(defalias 'redo 'undo-tree-redo)
-(global-set-key (kbd "C-z") 'undo)
-
-;;; cua
-(cua-mode 1)
-
-;;; hide menu bar for terminal,
-;;; make shift up work on the termninal
-;; (cond
-;;  ((eq window-system 'x)
-;;   (menu-bar-mode 1))
-;;  (t
-;;   (define-key input-decode-map "\e[1;2A" [S-up])
-;;   (menu-bar-mode 0)))
-
-;;; copy previous line - Ctrl-d
 (defun duplicate-line()
+  "Copy a line to the next line, mimics intellij."
   (interactive)
   (move-beginning-of-line 1)
   (kill-line)
   (yank)
   (open-line 1)
-  (next-line 1)
+  (forward-line 1)
   (yank))
 
 (global-set-key (kbd "C-d") 'duplicate-line)
 
-;;; desktops
-;;  (desktop-save-mode 1)
-;;  (desktop-registry-auto-register)
-
-;;; close all buffers
-
 (defun close-all-buffers ()
+  "kill all buffers"
   (interactive)
   (mapc 'kill-buffer (buffer-list)))
 
-;;; node 
-;;; find node
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-(setq exec-path
-      '(
-    "/usr/local/bin"
-    "/usr/bin"
-    "/bin"
-    ))
 
-;;;jshint
-(add-to-list 'load-path "/Users/r/.nvm/v0.10.28/lib/node_modules/jshint-mode")
-(require 'flymake-jshint)
-(add-hook 'javascript-mode-hook
-	  (lambda () (flymake-mode t)))
-
-;;; yaml mode
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-
-;;; org-files
-(setq org-todo-keywords
-           '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")))
-
-(setq org-agenda-files '("~/Desktop/todo"))
-(setq org-log-done 'time)
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
-
-;;; visual line mode
-(global-visual-line-mode t)
-
-;;; haskell
-(custom-set-variables
- '(haskell-mode-hook '(turn-on-haskell-indentation)))
-
-;;; server
-(server-start)
-
-;;; projectile
-(projectile-global-mode)
-(setq projectile-switch-project-action 'helm-projectile)
-
-
-;;; helm
-(require 'helm-config)
-(helm-mode 1)
-(helm-autoresize-mode 1)
-(setq helm-buffers-fuzzy-matching t)
-(setq helm-recentf-fuzzy-matching t)
-(setq helm-M-x-fuzzy-match t)
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-
-;;; company mode
-(add-hook 'after-init-hook 'global-company-mode)
-(global-set-key (kbd "s-/") 'company-complete)
-
-;;; recentf
-(recentf-mode t)
-
-;;; perspective
-(persp-mode)
-(require 'persp-projectile)
-(define-key projectile-mode-map (kbd "s-s") 'projectile-persp-switch-project)
-
-;;; helm projectile
-(setq helm-projectile-fuzzy-match t)
-
-
-(require 'helm-projectile)
-(helm-projectile-on)
-(setq projectile-completion-system 'helm)
-
-;;; custom 
-;;; erc
-(add-to-list 'load-path "~/.emacs.d/custom")
-(require 'erc-config)
-
-;;; scala
-(require 'scala-mode2)
-(require 'sbt-mode)
-(require 'popup)
-(require 'scala-outline-popup)
-(require 'ensime)
-
-
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
-(add-hook 'scala-mode-hook
-	  '(lambda ()
-	     (local-set-key (kbd "RET") 'newline-and-indent)
-	     (local-set-key (kbd "<backtab>") 'scala-indent:indent-with-reluctant-strategy)
-	     (setq compilation-skip-threshold 1)
-	     (local-set-key (kbd "M-RET") 'comint-accumulate)
-	     (local-set-key (kbd "M-.") 'sbt-find-definitions)
-	     (local-set-key (kbd "C-x '") 'sbt-run-previous-command)
-	     ))
-
-
-(define-key popup-isearch-keymap (kbd "s-e") 'popup-isearch-cancel)
-(setq scala-outline-popup-select 'closest)
-
-;; debug on error
-;; (setq debug-on-error t)
-
-;; silver searcher
-(require 'ag)
-(require 'helm-ag)
-(defun projectile-helm-ag ()
-  (interactive)
-  (helm-ag (projectile-project-root)))
-
-;;; error navigation
-
-(global-set-key [f3] 'flycheck-previous-error)
-(global-set-key [f4] 'flycheck-next-error)
-
-;;;(provide 'init)
-;;; init ends here
-
-
-
-
-
-
-
-
+(provide 'init)
